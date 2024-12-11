69bbe14dca476cc3f390f19a5f250cbdd8e88464
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-11 13:52:35 +0530

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class a 
+{
+    int c;
+    String b;
+    a(int c,String b)
+    {
+        this.c=c;
+        this.b=b;
+
+    }
+    void display()
+    {
+        System.out.println((b+" "+c));
+    }
+}
+public class Main2
+{
+    public static void main(String[] args) {
+        a s=new a(678,"jaaker");
+        s.display();
+    }
+}
