242c86031d2e09c859b8326aa4fbb508c33630d6
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-11 13:44:47 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class a 
+{
+    int c;
+    String b;
+    a()
+    {
+        this.c=345;
+        this.b="hello shaaaz";
+
+    }
+    void display()
+    {
+        System.out.println((b+" "+c));
+    }
+}
+public class Main2
+{
+    public static void main(String[] args) {
+        a s=new a();
+        s.display();
+    }
+}
