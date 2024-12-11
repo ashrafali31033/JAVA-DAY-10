9beb3938d55b372fb650437294d485d72a9681f7
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-11 10:28:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main
+{
+    static String s()
+
+    
+    {
+        return"hello students";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+        
+}
+
